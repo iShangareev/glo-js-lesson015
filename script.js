2bedd2e95634901a4ed217fe21a6d8dc473e0605
Ильнар Шангареев <i.shangareyev@modulbank.ru>
2022-02-23 20:09:10 +0400

    lesson015

--- script.js
+++ script.js
@@ -9,30 +9,32 @@ const fontSize = inputs[4]
 const text = inputs[5]
 
 const DomElement = function (selector, height, width, bg, fontSize, text) {
-   this.selectorClass = selector.slice(1)
-   this.selectorId = selector.slice(1)
-
-   this.createEl = function () {
-    if (selector.charAt(0) === '.') {
-      const elementClass = document.createElement('div')
-      elementClass.classList.add(this.selectorClass)
-      box.append(elementClass)
-      elementClass.innerText = text
-      elementClass.style.cssText = `width:${width}px;height:${height}px;background-color:${bg};font-size:${fontSize}px`
-    } else if (selector.charAt(0) === '#') {
-      const elementId = document.createElement('p')
-      elementId.id = this.selectorId
-      box.append(elementId)
-      elementId.innerText = text
-      elementId.style.cssText = `width:${width}px;height:${height}px;background-color:${bg};font-size:${fontSize}px`
-    } else if (selector.charAt(0) !== '#' && selector.charAt(0) !== '.') {
-      alert('Класс или ID должны начинатся с соотвествующего символа')
-    }
+   this.selectorClass = selector.value.slice(1)
+   this.selectorId = selector.value.slice(1)
   }
-}
+
+  DomElement.prototype.createEl = function () {
+    console.log(selector.value)
+   if (selector.value.charAt(0) === '.') {
+     const elementClass = document.createElement('div')
+     elementClass.classList.add(this.selectorClass)
+     box.append(elementClass)
+     elementClass.innerText = text.value
+     elementClass.style.cssText = `width:${width.value}px;height:${height.value}px;background-color:${bg.value};font-size:${fontSize.value}px`
+   } else if (selector.value.charAt(0) === '#') {
+     const elementId = document.createElement('p')
+     elementId.id = this.selectorId
+     box.append(elementId)
+     elementId.innerText = text.value
+     elementId.style.cssText = `width:${width.value}px;height:${height.value}px;background-color:${bg.value};font-size:${fontSize.value}px`
+   } else if (selector.value.charAt(0) !== '#' && selector.value.charAt(0) !== '.') {
+     alert('Класс или ID должны начинатся с соотвествующего символа')
+   }
+ }
+
 
 button.addEventListener('click', () => {
-  const newEl = new DomElement(selector.value, height.value, width.value, bg.value, fontSize.value, text.value)
+  const newEl = new DomElement(selector, height, width, bg, fontSize, text)
   newEl.createEl()
 })
 
